8c19fecbc96b33ae5577368a30642d4367fbc971
ironmewto <falixserverminecraft@gmail.com>
2025-06-14 12:04:08 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mjolnir Mod
+This is the v0.8 source code for the Mjolnir Mod (Fabric 1.21.5).
---
+++ src/main/java/com/mjolnir/MjolnirMod.java
@@ -0,0 +1,6 @@
+package com.mjolnir;
+public class MjolnirMod {
+    public static void main(String[] args) {
+        System.out.println("Mjolnir Mod Loaded");
+    }
+}
